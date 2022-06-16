ce134de81682c28bfafdf952f902ceb81588e909
Léo Llorca <leo.llorca@hotmail.com>
2022-06-16 15:13:51 +0200

    Users can add, remove or edit concerts

---
+++ api/index.js
@@ -0,0 +1,95 @@
+const fastify = require("fastify");
+const cors = require("@fastify/cors");
+const mongoist = require("mongoist");
+
+const server = fastify({ logger: true });
+server.register(cors);
+
+function start() {
+  server
+    .listen(3000)
+    .then(function () {
+      console.log("Server is running");
+    })
+    .catch(function (err) {
+      console.log("Server is not running", err);
+    });
+}
+start();
+
+const db = mongoist(
+  "mongodb://mongo:weIHenK6pJrfuy1Oqv8C@containers-us-west-61.railway.app:7500"
+);
+db.runCommand({ ping: 1 });
+db.on("connect", () => {
+  console.log("Database connected");
+});
+db.on("error", (err) => {
+  console.log("Database not connected", err);
+});
+
+server.get("/", (request, reply) => {
+  reply.send("Welcome on Alain Llorca's concerts API !");
+});
+
+server.post("/concerts", (request, reply) => {
+  const concert = request.body;
+  db.concerts
+    .insert(concert)
+    .then(() => {
+      reply.code(201).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      reply.code(500).send();
+    });
+});
+
+server.get("/concerts", (request, reply) => {
+  db.concerts
+    .find()
+    .then((res) => {
+      reply.send(res);
+    })
+    .catch(() => {
+      reply.code(500).send();
+    });
+});
+
+server.put("/concerts/:id", (request, reply) => {
+  const concertId = request.params.id;
+  const concert = request.body;
+  db.concerts
+    .update(
+      { _id: mongoist.ObjectId(concertId) },
+      {
+        $set: {
+          date: concert.date,
+          city: concert.city,
+          depNum: concert.depNum,
+          place: concert.place,
+          ticketsLink: concert.ticketsLink,
+        },
+      }
+    )
+    .then(() => {
+      reply.code(201).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      reply.code(500).send();
+    });
+});
+
+server.delete("/concerts/:id", (request, reply) => {
+  const concertId = request.params.id;
+  db.concerts
+    .remove({ _id: mongoist.ObjectId(concertId) })
+    .then(() => {
+      reply.code(201).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      reply.code(500).send();
+    });
+});
